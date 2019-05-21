e793dc0ae17ff44a3409314d6d5f75ebd89b6359
lhdurante <50871344+lhdurante@users.noreply.github.com>
2019-05-21 09:22:58 -0300

    Update ejercicio2.js
    Casado Montero

---
+++ ejercicio2.js
@@ -0,0 +1,12 @@
+var name= "Lucho Heguy"
+var age=23
+var gender="mujer"
+var ismember= "SI"
+var thirsty="si"
+var drinkName="Cunnington light"
+var person=prompt("hola como te llamas",name)
+var person=prompt("cuantos años tenes",age)
+var person=prompt("cual es tu genero",gender)
+var person=prompt("sos miembro",ismember)
+var person=prompt("tenes sed",thirsty)
+var person=prompt("que bebida queres",drinkName)
